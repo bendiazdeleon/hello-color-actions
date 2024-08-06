0f76fa60eb033b814e26e6e0cdc3837eebb83294
bendiazdeleon <bendiazdeleon@gmail.com>
2024-08-05 19:11:06 -0600

    Mi primer cambio en hello.js

--- hello.js
+++ hello.js
@@ -1,4 +1,4 @@
-const hello = () => "Hello World from the Office Hours in the terminal!";
+const hello = () => "Hello World. Benjamín from México City";
 console.log(hello());
 exports.hello = hello;
 
